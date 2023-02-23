5e48ce144348e1bfc0a4af9ca80689afa59ba79b
Jack Willars <jackwillars@jacks-mbp.home>
2023-02-23 22:46:43 +0000

    initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Toaster />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Helvetica 65 Medium', sans-serif`,
+  },
+});
+
+export default theme;
